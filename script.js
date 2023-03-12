1ce9d5e0f1618c62d3702d62fdd04835984d17b2
israeldail <israeldail2@gmail.com>
2023-03-11 20:55:25 -0500

    it draws

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 const wrapper = document.querySelector('.wrapper');
+const clear = document.querySelector('.newGrid');
 
-function createTable() {
-  for (let i = 0; i < 192; i += 1) {
-    const createDiv = document.createElement('div');
-    wrapper.appendChild(createDiv);
+clear.style.background = "white"
+clear.style.marginBottom = "15px"
+clear.style.cursor = "pointer"
+
+function makeGrid(screenSize) {
+  for (let i = 0; i < screenSize * screenSize; i++) {
+    let square = document.createElement('div');
+    square.classList.add("square");
+    wrapper.appendChild(square);
+
+    //change background color of a square on hover
+    square.addEventListener('mouseover', e => {
+      square.style.backgroundColor = "black";
+    })
+
+
+    //function to reset the grid
+    function clearGrid() {
+      clear.addEventListener('click', e => {
+        square.style.backgroundColor = "white"
+      })
+    }
+    clearGrid();
   }
+  wrapper.style.gridTemplateColumns = `repeat(${screenSize}, 1fr)`;
+  wrapper.style.gridTemplateRows = `repeat(${screenSize}, 1fr)`;
+
 }
 
-createTable();
+makeGrid(36);
+
+
+
+
+
+
